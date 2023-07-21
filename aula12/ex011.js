78f1ce625a74d7d172bb0e181a403aac984a1096
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-07-21 10:40:35 -0300

    ex11  js

---
+++ aula12/ex011.js
@@ -0,0 +1,12 @@
+var idade = 12
+
+if (idade >= 18){
+    console.log('Voto Obrigatório')
+
+} else {
+    if (idade >= 16 && idade < 18){
+        console.log('Voto Opcional')
+    } else if (idade < 16) {
+        console.log ('Não vota')
+    } 
+}
