e369513c12b35f92cbe1f8e613de10f645917699
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-07-21 11:29:25 -0300

    switch exercise js

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,5 +1,6 @@
-var hora = 20
-
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas`)
 if (hora < 12){
     console.log('Bom dia!')
 } else if (hora < 19){
