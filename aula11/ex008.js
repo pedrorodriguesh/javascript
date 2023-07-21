c2dbb6ddc9d6ad463e2efd17332b8d49874b9c43
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-07-21 00:14:56 -0300

    exercise js

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 65
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+    if (vel > 60){ // Condição Simples! 
+        console.log('Você ultrapassou a velocidade permitida. MULTADO')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'EUA'){
+    console.log('Americano')
+} else {
+    console.log('Estrangeiro')
+}
