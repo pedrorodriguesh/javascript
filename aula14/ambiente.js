df61c09ea0c1f2026eaba380dbb795f6ab90696d
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-07-22 15:36:37 -0300

    for

---
+++ aula14/ambiente.js
@@ -0,0 +1,6 @@
+console.log('Vai começar....')
+for (cont = 1; cont <= 10; cont++){  // Aqui é o loop for: inicialmente a gente tem (INICIO; TESTE; INCREMENTO)
+    console.log(`Passo ${cont}`)
+}
+
+console.log('FIM')
