ec3b819d00a0243a0de9b8198e6caa474df00c15
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-07-25 15:52:30 -0300

    votos update

--- exerciciopessoal/js/scriptvotos.js
+++ exerciciopessoal/js/scriptvotos.js
@@ -2,7 +2,10 @@ function enviar(){
     var txtname = document.querySelector('input#nome')
     var txtage = document.querySelector('input#age')
     var age = Number(txtage.value)
+    var name = String(txtname.value)
     var res = document.querySelector('div#res')
+    var dname = document.querySelector('div#name')
+    dname.innerHTML = `Tudo certo <strong>${name}</strong> vamos ver sua situação:`
     
     if (age < 16){
         res.innerHTML = `Com ${age} anos você <strong>não vota!</strong>`
