86fbc595a25b51f1b0a6817612f61674babac49a
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-07-25 15:07:28 -0300

    sistema de multa js terminado

--- exerciciopessoal/js/scriptmulta.js
+++ exerciciopessoal/js/scriptmulta.js
@@ -1,11 +1,29 @@
 function enviar(){
 var txtvel = document.querySelector('input#vel')
 var res = document.querySelector('div#res')
+var situ = document.querySelector('div#situ')
 var vel = Number(txtvel.value)
-res.innerHTML = `A velocidade registrada foi de <strong>${vel} Km/h.</strong>`
 
-    if (vel > 70){
-        res.innerHTML = `Você foi Multado!`
+if (vel > 70){
+    
+        res.innerHTML = `A velocidade registrada foi de <strong>${vel} Km/h.</strong>`
+        situ.innerHTML = `Você foi <strong>Multado!</strong>`
+
+    } else if (vel < 10) {
+    
+        window.alert("[ERRO] Insira um valor válido.")
+        res.innerHTML = `Insira um valor válido!`
+
+    } else {
+
+        res.innerHTML = `A velocidade registrada foi de <strong>${vel} Km/h.</strong>`
+        situ.innerHTML = `Você está trafegando dentro do limite, tenha uma boa viagem!`
+
     }
+
+        
+     
+
+   
 
 }
