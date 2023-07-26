f4e7b12b8a10892c59e3c2cddb3736ec68caa6a2
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-07-26 16:19:14 -0300

    array

---
+++ aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5, 4, 7]
+
+num.sort()  // num.sort deixa os valores em ordem crescente
+num.push(10) // num.push ele cria um novo valor dentro do valor, criando um novo espaço, nesse caso é o espaço [3]
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)   // num.length diz o tamanho do vetor
+console.log(`O primeiro valor do vetor é  ${num[0]}`) // num com [] na frente diz qual o valor está dentro de tal posição
