5f01e9cdae048ee74dd513ed16d8a3f303e69238
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-07-25 21:44:42 -0300

    teste

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,17 +1,6 @@
 var cont = 1
-do {
-    console.log(`Passo ${cont}`)     // Estrutura lógica com o laço de repetição no final
-    cont++ 
-}   while (cont <= 10)
 
-
-
-
-
-/* 
-var cont = 1
-while (cont <= 60000){
-    console.log(`Passo ${cont}`)     Estrutura lógica com o laço de repetição no começo 
-    cont++ 
+while (cont < 10){
+    console.log('Tudo bem?')
+    cont++
 }
-*/
