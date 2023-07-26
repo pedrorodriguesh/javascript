6a165a9c81efafa5bd0739050c97cb2f3247b67e
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-07-26 11:00:51 -0300

    exercicio for

---
+++ projeto03/js/script.js
@@ -0,0 +1,26 @@
+function contar(){
+    var txtini = document.querySelector('input#inicio')
+    var txtfim = document.querySelector('input#test')
+    var txtpasso = document.querySelector('input#passo')
+    var res = document.querySelector('div#res')
+
+
+    if (txtini.value.length == 0 || txtfim.value.length == 0 || txtpasso.value.length == 0){
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        var i = Number(txtini.value)
+        var f = Number(txtfim.value)
+        var p = Number(txtpasso.value)
+
+
+        for (var c = i; c <= f; c+= p){
+            res.innerHTML += `${c} `
+        }
+    }
+
+    
+
+
+
+}
