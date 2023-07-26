f9aafc2fa7a62e2675acb89c1751b739f224144c
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-07-26 16:44:19 -0300

    a

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -5,6 +5,6 @@ if (hora < 12){
     console.log('Bom dia!')
 } else if (hora < 19){
     console.log('Boa Tarde!')    
-} else if (hora > 19){
+} else if  (hora > 19){
     console.log ('Boa noite!')
 }
