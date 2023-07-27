d770c85f7551684669eb67f227c375451657ba7a
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-07-27 19:20:16 -0300

    functions

--- exercicioarrayfun/js/script.js
+++ exercicioarrayfun/js/script.js
@@ -5,25 +5,31 @@ let valores = []
 
 
 function isNumero(n){
-
-
+    if (Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
 }
 
 
-function inLista(n){
-
-    
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    } else {
+        return false
+    }
 }
-
-
-
 
 
 
 
 function adicionar() {
-    if(isNumero(num.value) && !inLista(num.value)) {
-
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
 
     } else {
         window.alert('Valor inválido ou já encontrado na lista!')
