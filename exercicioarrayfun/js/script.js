400cdd595760ccd6a71a78abae3bbf957b862cbb
Pedro Henrique Rodrigues <139188735+pedrorodriguesh@users.noreply.github.com>
2023-07-28 00:29:59 -0300

    comments

--- exercicioarrayfun/js/script.js
+++ exercicioarrayfun/js/script.js
@@ -14,7 +14,7 @@ function isNumero(n){                              // função para ver se o nú
 
 
 function inLista(n, l){
-    if (l.indexOf(Number(n)) != -1){
+    if (l.indexOf(Number(n)) != -1){ // Aqui ele verifica se o número informado já existe no vetor, esse  -1 significa = Não existe.
         return true
     } else {
         return false
@@ -22,12 +22,12 @@ function inLista(n, l){
 }
 
 
-function adicionar() {
-    if(isNumero(num.value) && !inLista(num.value, valores)) {
+function adicionar() {                                         // Chamando a função com o "onclick" e ai essa mesma função chama as funções de cima.
+    if(isNumero(num.value) && !inLista(num.value, valores)) { // Aqui as funções de cima são chamadas. (isNumero e inLista)
         valores.push(Number(num.value))
-        let item = document.createElement('option')
+        let item = document.createElement('option')    // Aqui a variável "item" é criada sendo um elemento option dentro do HTML.
         item.text = `Valor ${num.value} adicionado`
-        lista.appendChild(item)
+        lista.appendChild(item) // appendChild ele tá criando um "filho" dentro da lista, esse filho é a variável "item" que no caso é um option.
 
         res.innerHTML = ''
 
@@ -35,17 +35,17 @@ function adicionar() {
         window.alert('Valor inválido ou já encontrado na lista!')
     }
     num.value = ''
-    num.focus()
+    num.focus() // Comando usado pra seleção ficar no input
 
 
 }
 
 
-function finalizar(){
-    if (valores.length == 0){
+function finalizar(){                   // Essa é a função que é chamada quando o segundo botão é apertado, para checar os valores.
+    if (valores.length == 0){     // Aqui ele checa o tamanho do vetor, se estiver vazio ele manda o alert.
         window.alert('Adicione valores antes de finalizar!')
     } else {
-        let tot = valores.length
+        let tot = valores.length    // Aqui é bem simples, valores.length, essa propriedade length devolve o tamanho do vetor. 
         let maior = valores[0]
         let menor = valores [0]
         let soma = 0 
